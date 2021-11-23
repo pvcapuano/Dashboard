fab8349a13f48224e15ad6f3e665df1c69ad44eb
pvcapuano <pvcapuano@gmail.com>
2021-11-23 20:10:39 -0200

    install dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React , { useState } from 'react'
 import './App.css';
+import Chart from 'react-google-charts'
 
 function App() {
+
+  const TITULO = "Quantidade de cadastros primeiro semestre"
+
+  const [ dados, setDados ] = useState([
+    ['Mes', 'Quantidade'],
+    ['Janeiro', 20],
+    ['Fevereiro', 30],
+    ['Março', 40],
+    ['Abril', 50],
+    ['Maio', 60],
+    ['Junho', 70]
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+    <h1>Dashboard</h1>
+    <Chart
+    width={'400px'}
+    height={'300px'}
+    chartType={'PieChart'}
+    data={dados}
+    options={{
+      title: TITULO
+    }}
+    />
+
+<Chart
+    width={'400px'}
+    height={'300px'}
+    chartType={'PieChart'}
+    data={dados}
+    options={{
+      title: TITULO,
+      is3D: true
+    }}
+    />
+
+<Chart
+    width={'400px'}
+    height={'300px'}
+    chartType={'PieChart'}
+    data={dados}
+    options={{
+      title: TITULO,
+      pieHole: 0.3
+    }}
+    />
+
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Deve renderizar o componente', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Dashboard/i);
   expect(linkElement).toBeInTheDocument();
 });
